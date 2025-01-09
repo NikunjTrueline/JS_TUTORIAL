f711cd571c900129e9fbacab147cc253b434cd43
Nikunj Asodariya <truelineinstitute.info@gmail.com>
2025-01-09 16:22:34 +0530

    BASIC COMPLATE

---
+++ advance_js/error_object.js
@@ -0,0 +1,27 @@
+// try{
+//     nikunj
+// }catch(error)
+// {
+//     console.log(error.name)
+//     console.log(error.message)
+// }
+
+
+try{
+    throw new Error("Yes This is my custom Error")
+}catch(error)
+{
+   console.log(error.name)
+   console.log(error.message)
+}
+
+
+
+
+
+
+
+
+
+
+
